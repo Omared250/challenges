4a7d47161ac9641172f8df3967682904919485dd
Omar <omared250@gmail.com>
2021-10-03 22:25:10 +0200

    challenge 4 completed

--- challenge#4/script.js
+++ challenge#4/script.js
@@ -26,3 +26,10 @@ Hints:
 
 GOOD LUCK 😀
 */
+const bill = 275;
+// const bill = 40;
+// const bill = 430;
+
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value is ${bill + tip}`);
