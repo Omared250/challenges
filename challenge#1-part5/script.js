d0e9c00c88b7da930444dea2cc4a97dfe3651375
Omar <omared250@gmail.com>
2022-01-31 12:31:32 +0100

    challenge finished

--- challenge#1-part5/script.js
+++ challenge#1-part5/script.js
@@ -60,30 +60,31 @@ const poll = {
     // This generates [0, 0, 0, 0]. More in the next section!
     answers: new Array(4).fill(0),
     registerNewAnswer() {
-        // Get the answer
+        // Getting the answer
         const answer = Number(
             prompt(`${this.question}\n${this.options.join('\n')}
-            \n(Write option number)`));
-        // Register the answer
-        typeof answer === 'number' && answer < this.options.length && this.answers[answer]++;
+            \n(Write number option)`)
+        )
+        // Registering the answer
+        typeof answer === 'number' && 
+        answer < this.options.length &&
+        this.answers[answer]++;
 
         this.displayResults();
-        this.displayResults('string')
+        this.displayResults('string');
     },
     displayResults(type = 'array') {
         if (type === 'array') {
             console.log(this.answers);
         } else if (type === 'string') {
-            console.log(`Poll results are ${this.answers.join(', ')}`);
+            console.log(`Poll results are: ${this.answers.join(', ')}`);
         }
     }
 };
 
-// poll.registerNewAnswer();
-document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
 
 // Bonus
-poll.displayResults.call({ answers : [5, 2, 3]})
-poll.displayResults.call({ answers : [5, 2, 3]}, 'string')
-poll.displayResults.call({ answers : [1, 5, 3, 9, 6, 1]})
-poll.displayResults.call({ answers : [1, 5, 3, 9, 6, 1]}, 'string')
+poll.displayResults.call({answers: [5, 2, 3]});
+poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]})
+poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]}, 'string')
