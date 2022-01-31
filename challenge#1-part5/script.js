e504d45975d313b7851e8cde834863e0865ce258
Omar <omared250@gmail.com>
2022-01-31 11:29:11 +0100

    get the answer and registering the answer

--- challenge#1-part5/script.js
+++ challenge#1-part5/script.js
@@ -59,4 +59,16 @@ const poll = {
     options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
     // This generates [0, 0, 0, 0]. More in the next section!
     answers: new Array(4).fill(0),
+    registerNewAnswer() {
+        // Get the answer
+        const answer = Number(
+            prompt(`${this.question}\n${this.options.join('\n')}
+            \n(Write option number)`));
+        // Register the answer
+        typeof answer === 'number' && answer < this.options.length && this.answers[answer]++;
+        
+        console.log(this.answers);
+    }
 };
+
+poll.registerNewAnswer();
