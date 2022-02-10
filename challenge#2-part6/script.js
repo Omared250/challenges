b64e94fec6d3ed882e21fe1cd557e93201cd533f
Omar <omared250@gmail.com>
2022-02-10 16:37:54 +0100

    challenge completed

--- challenge#2-part6/script.js
+++ challenge#2-part6/script.js
@@ -28,3 +28,17 @@ Test data:
 § Data 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀 */
+
+const calcAverageHumanAge = function(ages) {
+
+    const humanAge = ages.map(dogAge => dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4)
+    .filter(humanAge => humanAge >= 18)
+    .reduce((acc, age, i, arr) => {
+        return acc + age / arr.length;
+    }, 0)
+
+    return `The average of the dogs in human ages is: ${humanAge}`;
+};
+
+// console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
