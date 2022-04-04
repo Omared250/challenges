9ecfc95aa667e0cdd2f0a5e007f6c37be85e5b9e
Omar <omared250@gmail.com>
2022-04-04 21:19:04 +0200

    challenge 2 part 7 OOP completed

--- challenge#2-part7/script.js
+++ challenge#2-part7/script.js
@@ -20,3 +20,37 @@ Data car 1: 'Ford' going at 120 km/h
 
 GOOD LUCK 😀
 */
+
+class CarCl {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} going at ${this.speed}km/h`);
+    }
+
+    break() {
+        this.speed -= 5;
+        console.log(`${this.make} going at ${this.speed}km/h`);
+    }
+
+    get speedUs() {
+        return this.speed / 1.6;
+    }
+
+    set speedUs(speed) {
+        return this.speed = speed * 1.6;
+    }
+}
+
+const ford = new CarCl('ford', 120);
+console.log(ford.speedUs);
+ford.accelerate();
+ford.accelerate();
+ford.break();
+ford.speedUs = 50;
+console.log(ford);
+ford.accelerate();
