054a0f4b2a201efe9d2560350455df3948e54151
Omar <omared250@gmail.com>
2022-04-25 11:36:21 +0200

    fetching data using promises

--- challenge#1-part8/script.js
+++ challenge#1-part8/script.js
@@ -49,3 +49,13 @@ Test data:
 § Coordinates 3: -33.933, 18.474
 
 GOOD LUCK 😀 */
+
+// Where am I function
+
+const whereAmI = function(lat, lng) {
+    fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+whereAmI(52.508, 13.381);
