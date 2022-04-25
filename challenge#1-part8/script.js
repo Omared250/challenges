7778e8d43ddf6487d14d00d90f2340bb4f06990a
Omar <omared250@gmail.com>
2022-04-25 11:42:54 +0200

    printing to the console the city and the country

--- challenge#1-part8/script.js
+++ challenge#1-part8/script.js
@@ -55,7 +55,12 @@ GOOD LUCK 😀 */
 const whereAmI = function(lat, lng) {
     fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        const strLocation = `You are in ${data.city}, ${data.country}`
+        console.log(strLocation);
+    })
 }
 
 whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
